4889eebc34a3a287c70b1905768120dc85e0fd35
Maurício Da Silva Pires <mauricio.spires@yahoo.com.br>
2016-10-27 13:28:21 -0200

    Create proj.c

---
+++ proj.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char[] processos ={'l','e','l','l','l','e'};
+    int i =0, tam=5;
+    while(i<tam){
+        printf("%c",pr)
+        i++;
+    }
+	// your code goes here
+	return 0;
+}
