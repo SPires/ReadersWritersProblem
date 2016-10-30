fa24472255bb8257bfc85988017a32f2a9b8b32a
RodrigoLugao <rodrigolugao@mail.com>
2016-10-30 21:22:31 -0200

    Update proj.c

--- proj.c
+++ proj.c
@@ -1,13 +1,42 @@
-#include<stdio.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define NUM_THREADS     5
 
-\\ Definir a criação de uma thread
+ void *leitor(void *argumento){
+    //le
+	printf("Lendo\n");
+    pthread_exit(NULL);
+ }
+ 
+  void *escritor(void *argumento){
+    //escreve
+	printf("Escrevendo\n");
+    pthread_exit(NULL);
+ }
 
-\\ Definir o tipo de uma determinada thread
+ int main (int argc, char *argv[]){
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long i;
+	int j = 0;
+	char arquivo[] = "teste.txt";
+	
+    for(i=0; i<NUM_THREADS; i++){
+       printf("In main: creating thread %ld\n", i);
+	   if(j == 2){
+			rc = pthread_create(&threads[i], NULL, escritor, (void *)i);
+			j = 0;
+	   }else{
+		   rc = pthread_create(&threads[i], NULL, leitor, (void *)i);
+		   j++;
+	   }
+       if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+       }
+    }
 
-\\ Criar a main associada ao programa
-
-int main (void){
-
-return 1;
-}
+    pthread_exit(NULL);
+	return 0;
+ }
